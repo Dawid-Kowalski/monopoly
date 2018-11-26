86764fc868df60706fb5cd9b95f3870361613b0e
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-11-26 22:20:03 +0100

    Add fields in corners

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Canvas */
+/* canvas */
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -6,9 +6,18 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-/* skróty */
+/* skróty wysokości i szerokości*/
 const cw = canvas.width;
 const ch = canvas.height;
+
+/* wielkości pól */
+const longSide = 100;
+const smallSide = (cw - longSide*2)/9; // po odjęciu 2 szerokości kwadratów w rogach na planszy musi się zmieścić 9 pól
+
+/* skróty wielkości pól */
+const lS = longSide;
+const sS = smallSide;
+
 
 /*plansza tło */
 let backgroundColor = "palegreen";
@@ -16,4 +25,27 @@ let backgroundColor = "palegreen";
 /* rysowanie planszy - tło*/
 ctx.fillStyle = backgroundColor;
 ctx.fillRect(0,0,cw,ch);
+
+
+/* rysowanie pól w rogach */
+
+/* lewy górny */  
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, lS, lS);
+ctx.strokeRect(0, 0, lS, lS);
+
+/* prawy górny */
+ctx.fillStyle = "white";
+ctx.fillRect(cw-lS ,0, lS, lS);
+ctx.strokeRect(cw-lS, 0, lS, lS);
+
+/* lewy dolny */
+ctx.fillStyle = "white";
+ctx.fillRect(0, ch-lS, lS, lS);
+ctx.strokeRect(0, ch-lS, lS, lS);
+
+/* prawy dolny */
+ctx.fillStyle = "white";
+ctx.fillRect(cw - lS, ch-lS, lS, lS);
+ctx.strokeRect(cw - lS, ch-lS, lS, lS);
 
