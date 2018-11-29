e7937456bfeea46ee083fa745c8700d1cad93bbf
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-11-29 12:19:42 +0100

    Add colors inputs for players

--- js/hiddenplayerssettingsfunctions.js
+++ js/hiddenplayerssettingsfunctions.js
@@ -1,26 +1,29 @@
 function showPlayerFields() {
 
 	if(numberOfPlayers == "2"){
-		hidePlayersSettings("player3-name-label", "player3-name", true);
-		hidePlayersSettings("player4-name-label", "player4-name", true);
+		hidePlayersSettings("player3-name-label", "player3-name", "player3-color-label", "player3-color", true);
+		hidePlayersSettings("player4-name-label", "player4-name", "player4-color-label", "player4-color", true);
 	}
 
 	if(numberOfPlayers == "3"){
-		hidePlayersSettings("player3-name-label", "player3-name", false);
-		hidePlayersSettings("player4-name-label", "player4-name", true);
+		hidePlayersSettings("player3-name-label", "player3-name", "player3-color-label", "player3-color", false);
+		hidePlayersSettings("player4-name-label", "player4-name", "player4-color-label", "player4-color", true);
 	}
 
 	if(numberOfPlayers == "4"){
-		hidePlayersSettings("player3-name-label", "player3-name", false);
-		hidePlayersSettings("player4-name-label", "player4-name", false);
+		hidePlayersSettings("player3-name-label", "player3-name", "player3-color-label", "player3-color", false);
+		hidePlayersSettings("player4-name-label", "player4-name", "player4-color-label", "player4-color", false);
 	}
 }
 
-function hidePlayersSettings (label, input, ishidden) {
+function hidePlayersSettings (label, inputText, labelColor, inputColor, ishidden) {
 		document.getElementById(label).hidden = ishidden;
+		document.getElementById(labelColor).hidden = ishidden;
 		if(ishidden) {
-			document.getElementById(input).type = "hidden";
+			document.getElementById(inputText).type = "hidden";
+			document.getElementById(inputColor).type = "hidden";
 		} else {
-			document.getElementById(input).type = "text";
+			document.getElementById(inputText).type = "text";
+			document.getElementById(inputColor).type = "color";
 		}
 }
