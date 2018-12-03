9cc2e16587ac543ddee6e6f3873bb539a5b67c77
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-03 15:19:19 +0100

    Add buy house function

--- js/script.js
+++ js/script.js
@@ -1223,3 +1223,247 @@ function sellCity (id) {
 	buttonDisabled("buttonmortage"+id);
 	buttonDisabled("buttonmortageremove"+id);
 }
+
+function buyHouse (id) {
+
+	let player = activePlayer;
+	let fieldId = players[player].city[id-1].idField;
+	let canBuy = true;
+
+	if(players[player].city[id-1].idField == 0 && players[player].greeceComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - grecja");
+		canBuy = false;
+	}
+
+	if(id == 1 && players[player].city[0].house > players[player].city[1].house) {
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 2 && players[player].greeceComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - grecja");
+		canBuy = false;
+	}
+
+	if(id == 2 && players[player].city[1].house > players[player].city[0].house) {
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 5 && players[player].italyComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - włochy");
+		canBuy = false;
+	}
+
+	if(id == 3 && (players[player].city[2].house == Math.max(players[player].city[2].house, players[player].city[3].house, players[player].city[3].house)) && !(players[player].city[2].house==players[player].city[3].house && players[player].city[3].house==players[player].city[4].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 7 && players[player].italyComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - włochy");
+		canBuy = false;
+	}
+
+	if(id == 4 && (players[player].city[3].house == Math.max(players[player].city[2].house, players[player].city[3].house, players[player].city[4].house)) && !(players[player].city[2].house==players[player].city[3].house && players[player].city[3].house==players[player].city[4].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 8 && players[player].italyComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - włochy");
+		canBuy = false;
+	}
+
+	if(id == 5 && (players[player].city[4].house == Math.max(players[player].city[2].house, players[player].city[3].house, players[player].city[4].house)) && !(players[player].city[2].house==players[player].city[3].house && players[player].city[3].house==players[player].city[4].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 10 && players[player].spainComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - hiszpania");
+		canBuy = false;
+	}
+
+	if(id == 6 && (players[player].city[5].house == Math.max(players[player].city[5].house, players[player].city[6].house, players[player].city[7].house)) && !(players[player].city[5].house==players[player].city[6].house && players[player].city[5].house==players[player].city[7].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 12 && players[player].spainComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - hiszpania");
+		canBuy = false;
+	}
+
+	if(id == 7 && (players[player].city[6].house == Math.max(players[player].city[5].house, players[player].city[6].house, players[player].city[7].house)) && !(players[player].city[5].house==players[player].city[6].house && players[player].city[5].house==players[player].city[7].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 13 && players[player].spainComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - hiszpania");
+		canBuy = false;
+	}
+
+	if(id == 8 && (players[player].city[7].house == Math.max(players[player].city[5].house, players[player].city[6].house, players[player].city[7].house)) && !(players[player].city[5].house==players[player].city[6].house && players[player].city[5].house==players[player].city[7].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 15 && players[player].englandComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - anglia");
+		canBuy = false;
+	}
+
+	if(id == 9 && (players[player].city[8].house == Math.max(players[player].city[8].house, players[player].city[9].house, players[player].city[10].house)) && !(players[player].city[8].house==players[player].city[9].house && players[player].city[8].house==players[player].city[10].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 17 && players[player].englandComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - anglia");
+		canBuy = false;
+	}
+
+	if(id == 10 && (players[player].city[9].house == Math.max(players[player].city[8].house, players[player].city[9].house, players[player].city[10].house)) && !(players[player].city[8].house==players[player].city[9].house && players[player].city[8].house==players[player].city[10].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 18 && players[player].englandComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - anglia");
+		canBuy = false;
+	}
+
+	if(id == 11 && (players[player].city[10].house == Math.max(players[player].city[8].house, players[player].city[9].house, players[player].city[10].house)) && !(players[player].city[8].house==players[player].city[9].house && players[player].city[8].house==players[player].city[10].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 20 && players[player].beneluxComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - benelux");
+		canBuy = false;
+	}
+
+	if(id == 12 && (players[player].city[11].house == Math.max(players[player].city[11].house, players[player].city[12].house, players[player].city[13].house)) && !(players[player].city[11].house==players[player].city[12].house && players[player].city[11].house==players[player].city[13].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 22 && players[player].beneluxComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - benelux");
+		canBuy = false;
+	}
+
+	if(id == 13 && (players[player].city[12].house == Math.max(players[player].city[11].house, players[player].city[12].house, players[player].city[13].house)) && !(players[player].city[11].house==players[player].city[12].house && players[player].city[11].house==players[player].city[13].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 23 && players[player].beneluxComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - benelux");
+		canBuy = false;
+	}
+
+	if(id == 14 && (players[player].city[13].house == Math.max(players[player].city[11].house, players[player].city[12].house, players[player].city[13].house)) && !(players[player].city[11].house==players[player].city[12].house && players[player].city[11].house==players[player].city[13].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 25 && players[player].swedenComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - szwecja");
+		canBuy = false;
+	}
+
+	if(id == 15 && (players[player].city[14].house == Math.max(players[player].city[14].house, players[player].city[15].house, players[player].city[16].house)) && !(players[player].city[14].house==players[player].city[15].house && players[player].city[14].house==players[player].city[16].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 26 && players[player].swedenComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - szwecja");
+		canBuy = false;
+	}
+
+	if(id == 16 && (players[player].city[15].house == Math.max(players[player].city[14].house, players[player].city[15].house, players[player].city[16].house)) && !(players[player].city[14].house==players[player].city[15].house && players[player].city[14].house==players[player].city[16].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 28 && players[player].swedenComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - szwecja");
+		canBuy = false;
+	}
+
+	if(id == 17 && (players[player].city[16].house == Math.max(players[player].city[14].house, players[player].city[15].house, players[player].city[16].house)) && !(players[player].city[14].house==players[player].city[15].house && players[player].city[14].house==players[player].city[16].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 30 && players[player].rfnComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - rfn");
+		canBuy = false;
+	}
+
+	if(id == 18 && (players[player].city[17].house == Math.max(players[player].city[17].house, players[player].city[18].house, players[player].city[19].house)) && !(players[player].city[17].house==players[player].city[18].house && players[player].city[17].house==players[player].city[19].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 31 && players[player].rfnComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - rfn");
+		canBuy = false;
+	}
+
+	if(id == 19 && (players[player].city[18].house == Math.max(players[player].city[17].house, players[player].city[18].house, players[player].city[19].house)) && !(players[player].city[17].house==players[player].city[18].house && players[player].city[17].house==players[player].city[19].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 33 && players[player].rfnComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - rfn");
+		canBuy = false;
+	}
+
+	if(id == 20 && (players[player].city[19].house == Math.max(players[player].city[17].house, players[player].city[18].house, players[player].city[19].house)) && !(players[player].city[17].house==players[player].city[18].house && players[player].city[17].house==players[player].city[19].house)){
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 36 && players[player].austriaComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - austria");
+		canBuy = false;
+	}
+
+	if(id == 21 && players[player].city[20].house > players[player].city[21].house) {
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(players[player].city[id-1].idField == 38 && players[player].austriaComplete == "nie") {
+		alert("nie możesz kupić - nie posiadasz całego państwa - austria");
+		canBuy = false;
+	}
+
+	if(id == 22 && players[player].city[21].house > players[player].city[20].house) {
+		alert("musisz rozwijać państwo równomiernie");
+		canBuy = false;
+	}
+
+	if(canBuy == true) {
+		if(players[player].money < fields[fieldId].cost1house) { 
+			alert("masz za mało pieniędzy");
+			} else {
+				players[player].city[id-1].house++;
+				players[player].houseAll++;
+				document.getElementById("houseid"+id).innerHTML = players[player].city[id-1].house;
+
+				fields[fieldId].house++;
+
+				updatePlayerMoney(player, -fields[fieldId].cost1house);
+				showRoundMainData;
+
+				buttonEnabled("buttonsellhouse"+id);
+			}
+		}
+}
